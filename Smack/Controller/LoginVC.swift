be018d65d7ef45599d6763d1d630e102e9251afe
Илья Маслиев <ilamasliev@Ilas-MacBook-Air.local>
2018-02-14 23:43:36 +0300

    loginVC things

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Илья Маслиев on 14/02/2018.
+//  Copyright © 2018 Masliev. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Илья Маслиев on 14/02/2018.
+//  Copyright © 2018 Masliev. All rights reserved.
+//
+
+import Foundation
